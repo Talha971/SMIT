d5a167b3e4218ccb22148befdfa7b0cafb7e66dd
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-10-19 00:57:38 +0500

    Add files via upload

--- ASSIGNMENTS/2) JS chp # 18-25/app.js
+++ ASSIGNMENTS/2) JS chp # 18-25/app.js
@@ -41,38 +41,97 @@
 
 // 7
 
-document.write("<p>");
-document.write("Counting: ");
+// document.write("<p>");
+// document.write("Counting: ");
 
-for (var i = 1; i < 20; i++) {
-  document.write(i + " ");
-}
-document.write("</p>");
+// for (var i = 1; i < 20; i++) {
+//   document.write(i + " ");
+// }
+// document.write("</p>");
 
-document.write("<p>");
-document.write("Reverse Countin: ");
-for (var i = 20; i > 1; i--) {
-  document.write(i + " ");
-}
-document.write("</p>");
+// document.write("<p>");
+// document.write("Reverse Countin: ");
+// for (var i = 20; i > 1; i--) {
+//   document.write(i + " ");
+// }
+// document.write("</p>");
 
-document.write("<p>");
-document.write("Even: ");
-for (var i = 2; i < 20; i++) {
-  document.write(i++ + " ");
-}
-document.write("</p>");
+// document.write("<p>");
+// document.write("Even: ");
+// for (var i = 2; i < 20; i++) {
+//   document.write(i++ + " ");
+// }
+// document.write("</p>");
 
-document.write("<p>");
-document.write("Odd: ");
-for (var i = 1; i < 20; i++) {
-  document.write(i++ + " ");
-}
-document.write("</p>");
+// document.write("<p>");
+// document.write("Odd: ");
+// for (var i = 1; i < 20; i++) {
+//   document.write(i++ + " ");
+// }
+// document.write("</p>");
 
-document.write("<p>");
-document.write("Series: ");
-for (var i = 2; i <= 20; i++) {
-  document.write(i++ + "k ");
+// document.write("<p>");
+// document.write("Series: ");
+// for (var i = 2; i <= 20; i++) {
+//   document.write(i++ + "k ");
+// }
+// document.write("</p>");
+
+// // 8
+
+// var arr = ["apple", "mango", "orange"];
+// var b = prompt("enter the required fruit");
+
+// for(var i=0;i<=arr.length;i++){
+//     if(b==arr[i]){
+//         console.log(b +" is availabel at "+i+" index");
+//         break;
+//     }
+// }
+// if(b!==arr[i]){
+// console.log("not available");
+// }
+
+// // 9
+// var A = [24, 53, 78, 91, 12];
+// var largest = A[0];
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] > largest) {
+//     largest = A[i];
+//   }
+// }
+// document.write("The largest number in the array is: " + largest);
+
+// // 10
+// var A = [24, 53, 78, 91, 12];
+// var smallest = A[0];
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] < smallest) {
+//     smallest = A[i];
+//   }
+// }
+// document.write("The smallest number in the array is: " + smallest);
+
+// // 11
+
+// var A = [24, 53, 78, 91, 12];
+// var largest = A[0];
+// var smallest = A[0];
+
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] > largest) {
+//     largest = A[i];
+//   }
+// }
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] < smallest) {
+//     smallest = A[i];
+//   }
+// }
+// document.write("The largest number in the array is: " + largest);
+// document.write("<br>The smallest number in the array is: " + smallest);
+
+// 12
+for(var i=1;i<=20;i++){
+    document.write(i*5+" ");
 }
-document.write("</p>");
