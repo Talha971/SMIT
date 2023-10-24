36f7f5776fff45cc2e3a46233f150ad10ce446c1
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-10-25 00:35:57 +0500

    Add files via upload
    assignment 2 complete

--- ASSIGNMENTS/2) JS chp # 18-25/app.js
+++ ASSIGNMENTS/2) JS chp # 18-25/app.js
@@ -201,8 +201,6 @@
 
 // }
 
-
-
 // // 20(a)
 
 // for (var i = 1; i <= 6; i++) {
@@ -212,10 +210,6 @@
 //   document.write("<br>");
 
 // }
-
-
-
-
 
 // // 20(b)
 
@@ -227,19 +221,210 @@
 
 // }
 
+// // 20(c)
+// for (var i = 7; i >= 1; i--) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write("*");
+//   }
+//   document.write("<br>");
 
-
-// 20(c)
-for (var i = 7; i >= 1; i--) {
-  for (var j = 1; j <= i; j++) {
-    document.write("*");
-  }
-  document.write("<br>");
-
-}
-
-
-
+// }
 
 // //////////////////////////////// chp # 18-25 ////////////////////////////////////////
 
+// // 1
+// var a=prompt("write first name");
+// var b=prompt("write last name");
+// var fname=a+" "+b;
+// document.write(fname);
+
+// // 2
+// var a=prompt("write favorite mobile name");
+// document.write(a.length);
+
+// // 3
+// var a = "Pakistani";
+// var b = a.search(prompt("enter the letter"));
+// document.write("index of letter is: "+b);
+
+// // 4
+// var a = "Hello world";
+// var b = a.lastIndexOf(prompt("enter the letter"));
+// document.write(b);
+
+// // 5
+// var a = "Hello world";
+// var b = a.charAt(4)
+// document.write(b);
+
+// // 6
+// var a=prompt("write first name");
+// var b=prompt("write last name");
+// var c=a.concat(" "+b);
+// document.write(c);
+
+// 7
+// var a = "Hello world";
+// var b= a.replace("Hello","chaman");
+// document.write(b);
+
+// // 8
+
+// var message = 'Ali and Sami are best friends. They play cricket and football together.';
+// var c= message.replace(/and/g,"&");
+// document.write(c);
+
+// // 9
+
+// var a = "123";
+// document.write(typeof a);
+// var b= parseInt(a);
+// document.write(typeof b);
+
+// // 10
+
+// var a=prompt('enter url:')
+// var b=a.slice(4);
+// document.write(b);
+
+// // 11
+// var a=prompt('enter any thing');
+// var b=a.toUpperCase();
+// document.write(b);
+
+// // 12
+// var a=prompt('enter any thing');
+// var b=a.toLowerCase();
+// document.write(b);
+
+// // 13
+// var a = prompt("enter any thing");
+// var b = (a.slice(0, 1).toUpperCase()) + (a.substring(2).toLowerCase());
+// document.write(b);
+// console.log(b)
+
+// // 14
+// var num = 35.36;
+// var numAsString = num.toString(); // Convert the number to a string
+// var numWithoutDot = numAsString.replace(".", ""); // Remove the dot
+
+// // Display the result in your browser
+// document.write(numWithoutDot);
+
+// // 15
+// var a="3";
+// var b="3";
+// document.write(a+b);
+
+// // 16
+// var a="3";
+// var b="3";
+// document.write(a-b);
+
+// // 17
+// // var a="khsadkjh"
+// var a = prompt("eneter a username");
+// var b = String.fromCharCode(33);
+// var c = String.fromCharCode(44);
+// var d = String.fromCharCode(46);
+// var e = String.fromCharCode(64);
+// if ((a === b) || (a === c) || (a === d) || (a === e)) {
+//   document.write("eneter valid value!");
+// }
+
+// 18
+
+// var items = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// function searchItem(item) {
+//   for (var i = 0; i < items.length; i++) {
+//     if (items[i].toLowerCase() === item.toLowerCase()) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// var userInput = prompt("Enter an item to search for:");
+// if (userInput !== null) { // Check if the user canceled the prompt
+//   if (searchItem(userInput)) {
+//     alert(userInput + " is available in the list.");
+//   } else {
+//     alert(userInput + " is not found in the list.");
+//   }
+// } else {
+//   alert("Search canceled or no input provided.");
+// }
+
+// // 19
+// var a = prompt("1");
+// var b = prompt("2");
+// if (a.length > b.length) {
+//   alert("a is greater");
+// } else if (a === b) {
+//   alert("both eual");
+// } else {
+//   alert("B is greater");
+// }
+
+// // 20
+// var isValid = false;
+
+// while (!isValid) {
+//   var userInput = prompt("Enter a password:");
+
+//   if (userInput === null) {
+//     alert("Password input canceled.");
+//     break;
+//   }
+
+//   if (
+//     userInput.length >= 6 &&
+//     !/^\d/.test(userInput) &&
+//     /[a-zA-Z]/.test(userInput) &&
+//     /\d/.test(userInput)
+//   ) {
+//     isValid = true;
+//     alert("Password is valid.");
+//   } else {
+//     alert("Password does not meet the requirements. Please enter a valid password.");
+//   }
+// }
+
+// // 21
+// var university = "University of Karachi";
+// var universityArray = university.split("");
+
+// for (var i = 0; i < universityArray.length; i++) {
+//   document.write(universityArray[i] + "<br>");
+// }
+
+
+// // 22
+// var a="kdasjk";
+// var b=a.slice(-1);
+// document.write(b);
+
+// // 23
+// var a = "the quick brown fox jumps over the lazy dog";
+// var b=a.split(" the ");
+// document.write(b.length);
+
+// 24
+var str = "Pakistan";
+var vowels = "aeiouAEIOU";
+var vowelCount = 0;
+var consonantCount = 0;
+
+for (var i = 0; i < str.length; i++) {
+  var char = str[i];
+  if (vowels.indexOf(char) !== -1) {
+    vowelCount++;
+  } else if (char.match(/[a-zA-Z]/)) {
+    consonantCount++;
+  }
+}
+
+document.write("String: " + str + "<br>");
+document.write("Vowels: " + vowelCount + "<br>");
+document.write("Consonants: " + consonantCount + "<br>");
